2e25e5fa8f5a5af59467ea3956e146c191f363a4
PedrodeAndradecf <pedro.cfandrade@gmail.com>
2026-06-01 19:29:01 -0300

    feat: db - docker

--- src/main/java/Turma/ProjetoBack2026/ProjetoBckOpenAcad2026Application.java
+++ src/main/java/Turma/ProjetoBack2026/ProjetoBckOpenAcad2026Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoBack2026Application {
+public class ProjetoBckOpenAcad2026Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoBack2026Application.class, args);
+		SpringApplication.run(ProjetoBckOpenAcad2026Application.class, args);
 	}
 
 }
--- src/main/java/Turma/ProjetoBack2026/entity/Cliente.java
+++ src/main/java/Turma/ProjetoBack2026/entity/Cliente.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.yaml.snakeyaml.events.Event;
-
 import java.util.ArrayList;
 import java.util.List;
 @Setter
